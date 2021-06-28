d33a2a18dc773dd715e418053ad6aeebdb6d372d
Andrew Orals <nd-0r@andreworals.com>
2021-06-28 00:37:53 -0500

    Only add particles after overlap in both axes

--- Builds/MacOSX/ParticleManager.cpp
+++ Builds/MacOSX/ParticleManager.cpp
@@ -12,7 +12,8 @@ namespace synchrony {
 void ParticleManager::AddParticle(const Particle& new_particle) {
   particles_.push_back(new_particle);
   
-  AxisAlignedBoundingBox* new_particle_box = new_particle.GetBoundingBox();
+  const AxisAlignedBoundingBox* new_particle_box =
+    new_particle.GetBoundingBox();
   InsertSorted(positions_x_, new_particle_box->bounds_x);
   InsertSorted(positions_y_, new_particle_box->bounds_y);
 }
@@ -55,9 +56,42 @@ void ParticleManager::ResolveCollisions() {
   }
 }
 
+void ParticleManager::SortAxisAndFindCandidates(std::vector<EndPoint*>& axis) {
+  for (size_t key_idx = 0; key_idx < axis.size(); ++key_idx) {
+    EndPoint* key = axis.at(key_idx);
+    
+    size_t swapper_idx = key_idx - 1;
+    while (axis[swapper_idx]->value > key->value && swapper_idx > 0) {
+      EndPoint* to_swap = axis[swapper_idx];
+      
+      if (key->is_min && !to_swap->is_min) {
+        // lower bound moves ahead another lower bound
+        if (!(key->owner->is_collision_candidate &&
+            key->owner->is_collision_candidate)) {
+          // these particle bounds haven't overlapped yet
+          key->owner->is_collision_candidate = true;
+          to_swap->owner->is_collision_candidate = true;
+        } else {
+          // particle bounds overlapped on the other axis
+          collision_candidate_pairs_.push_back(
+              std::pair<Particle*, Particle*>(key->owner, to_swap->owner));
+        }
+      }
+      
+      if (!key->is_min && to_swap->is_min) { // lower bound moves ahead of upper bound
+        key->owner->is_collision_candidate = false;
+        to_swap->owner->is_collision_candidate = false;
+      }
+      
+      axis[swapper_idx] = axis[key_idx];
+      axis[key_idx] = to_swap;
+    }
+  }
+}
+
 void ParticleManager::InsertSorted(std::vector<EndPoint*>& end_points,
                                    const std::pair<EndPoint*, EndPoint*>& bounds,
-                                   size_t start_idx = 0) {
+                                   size_t start_idx) {
   EndPoint* point_to_insert = (start_idx == 0) ? bounds.first : bounds.second;
   
   if (end_points.empty()) {
@@ -74,36 +108,6 @@ void ParticleManager::InsertSorted(std::vector<EndPoint*>& end_points,
       end_points.insert(iter, point_to_insert);
       
       if (start_idx == 0) InsertSorted(end_points, bounds, iter - end_points.begin());
-    }
-  }
-}
-
-void ParticleManager::SortAxisAndFindCandidates(std::vector<EndPoint*>& axis) {
-  for (size_t key_idx = 0; key_idx < axis.size(); ++key_idx) {
-    EndPoint* key = axis.at(key_idx);
-    
-    size_t swapper_idx = key_idx - 1;
-    while (axis[swapper_idx]->value > key->value && swapper_idx > 0) {
-      EndPoint* to_swap = axis[swapper_idx];
-      
-      if (key->is_min && !to_swap->is_min) { // lower bound moves ahead another lower bound
-        if (!(key->owner->is_collision_candidate &&
-            key->owner->is_collision_candidate)) {
-          // these particle bounds haven't overlapped yet
-          collision_candidate_pairs_.push_back(
-              std::pair<Particle*, Particle*>(key->owner, to_swap->owner));
-          key->owner->is_collision_candidate = true;
-          to_swap->owner->is_collision_candidate = true;
-        }
-      }
-      
-      if (!key->is_min && to_swap->is_min) { // lower bound moves ahead of upper bound
-        key->owner->is_collision_candidate = false;
-        to_swap->owner->is_collision_candidate = false;
-      }
-      
-      axis[swapper_idx] = axis[key_idx];
-      axis[key_idx] = to_swap;
     }
   }
 }
--- Builds/MacOSX/ParticleManager.hpp
+++ Builds/MacOSX/ParticleManager.hpp
@@ -5,29 +5,15 @@
 //  Created by Andrew Orals on 6/20/21.
 //
 
-#ifndef Physics_hpp
-#define Physics_hpp
+#pragma once
 
 #include <stdio.h>
 #include "Particle.hpp"
+#include "Utilities.hpp"
 
 namespace synchrony {
 
-struct AxisAlignedBoundingBox;
-
-struct EndPoint {
-  EndPoint(Particle* to_owner, float to_value, bool to_is_min) :
-    owner(to_owner), value(to_value), is_min(to_is_min) {}
-  
-  Particle* owner;
-  float value;
-  bool is_min;
-};
-
-struct AxisAlignedBoundingBox {
-  std::pair<EndPoint*, EndPoint*> bounds_x;
-  std::pair<EndPoint*, EndPoint*> bounds_y;
-};
+class Particle;
 
 class ParticleManager {
 public:
@@ -39,7 +25,7 @@ public:
   
 private:
   void SortAxisAndFindCandidates(std::vector<EndPoint*>& axis);
-  
+
   static void InsertSorted(std::vector<EndPoint*>& end_points,
                            const std::pair<EndPoint*, EndPoint*>& bounds,
                            size_t start_idx = 0);
@@ -54,5 +40,3 @@ private:
 };
 
 } // namespace synchrony
-
-#endif /* Physics_hpp */
